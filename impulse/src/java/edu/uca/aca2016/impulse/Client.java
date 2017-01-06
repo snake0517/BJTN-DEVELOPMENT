aacdb82ea62112bc1c5abeb1d2b53d75ade3f2cf
brela <brela@192.168.1.106>
2017-01-06 09:16:29 -0600

    Impulse

---
+++ impulse/src/java/edu/uca/aca2016/impulse/Client.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.impulse;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author brela
+ */
+public class Client implements Serializable {
+
+    private int Clientid;
+    private String FirstName;
+    private String LastName;
+    private String Address1;
+    private String Address2;
+    private String City;
+    private String State;
+    private String Zip;
+    private String Email;
+    private String Phone;
+    private String Status;
+
+    public int getClientid() {
+        return Clientid;
+    }
+
+    public void setClientid(int Clientid) {
+        this.Clientid = Clientid;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getAddress1() {
+        return Address1;
+    }
+
+    public void setAddress1(String Address1) {
+        this.Address1 = Address1;
+    }
+
+    public String getAddress2() {
+        return Address2;
+    }
+
+    public void setAddress2(String Address2) {
+        this.Address2 = Address2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+}
