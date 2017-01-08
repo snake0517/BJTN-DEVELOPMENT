ee3f093f5d5995eb5b71062772012e8c1ff42579
brela <brela@192.168.1.106>
2017-01-07 21:16:01 -0600

    Impulse

---
+++ impulse/src/java/edu/uca/aca2016/impulse/repository/ClientDAO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.impulse.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import edu.uca.aca2016.impulse.Client;
+
+/**
+ *
+ * @author brela
+ */
+public class ClientDAO {
+    JdbcTemplate template;
+    
+    public void setTemplate(JdbcTemplate template){
+        this.template = template;
+    }
+    
+    
+}
