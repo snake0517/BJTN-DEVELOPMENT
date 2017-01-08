34215f85af62642036ec506b094bf238babf7496
brela <brela@192.168.1.106>
2017-01-08 11:37:58 -0600

    Impulse

--- impulse/src/java/edu/uca/aca2016/impulse/repository/ClientDAO.java
+++ impulse/src/java/edu/uca/aca2016/impulse/repository/ClientDAO.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -22,11 +21,15 @@ import edu.uca.aca2016.impulse.Client;
  * @author brela
  */
 public class ClientDAO {
+
     JdbcTemplate template;
-    
-    public void setTemplate(JdbcTemplate template){
+
+    public void setTemplate(JdbcTemplate template) {
         this.template = template;
     }
-    
-    
+
+    public int save(Client client) {
+        String sql = "TBD";
+        return template.update(sql);
+    }
 }
