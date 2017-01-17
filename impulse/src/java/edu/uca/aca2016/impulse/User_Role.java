ea0ce0b5b91cde8cf7975af07b4ed3f2b1036e0d
brela <brela@192.168.1.106>
2017-01-16 19:30:06 -0600

    Adding User Roles

---
+++ impulse/src/java/edu/uca/aca2016/impulse/User_Role.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.impulse;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author brela
+ */
+public class User_Role implements Serializable {
+    private int user_role_id;
+    private String username;
+    private String role;
+
+    public int getUser_role_id() {
+        return user_role_id;
+    }
+
+    public void setUser_role_id(int user_role_id) {
+        this.user_role_id = user_role_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    
+    
+}
